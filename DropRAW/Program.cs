91e53d8d7ff414bd2583fad57fdf04a95826b107
TonyTonyQ <soldierq@msn.com>
2017-12-12 17:26:36 +0800

    Init Build

---
+++ DropRAW/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+using System.Runtime.InteropServices;
+
+namespace DropRAW
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+
+            app.HelpOption();
+            var optionSubject = app.Option("-s|--subject <SUBJECT>", "The subject", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                var subject = optionSubject.HasValue()
+                    ? optionSubject.Value()
+                    : "world";
+
+                Console.WriteLine($"Hello {subject}!");
+
+                Console.ReadLine();
+
+                return 0;
+            });
+
+
+            return app.Execute(args);
+        }
+    }
+}
